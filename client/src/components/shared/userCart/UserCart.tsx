fbf6f61b7e7247273fcc05480c6e699caa0ce206
Natabyc <nataliabyc01@gmail.com>
2024-01-21 23:14:58 +0100

    map and arrays not working yet

--- client/src/components/shared/userCart/UserCart.tsx
+++ client/src/components/shared/userCart/UserCart.tsx
@@ -1,16 +1,31 @@
-import { Grid, Button} from '@mui/material';
+import { Grid, Button, Stack, TextField} from '@mui/material';
 import { useState } from 'react';
-
 
 export default function UserCart() {
   //zorbic z mapem zeby byla lista
 
-  const item = {
-    itemPrice: 100, //tu trzeba z bazy pobrac?
-    amount: 3,
-    id: 1,
-    title: "dupa"
-  }
+  const [items, setItems] = useState(
+    [
+      {
+        itemPrice: 100, //tu trzeba z bazy pobrac?
+        amount: 3,
+        id: 1,
+        title: "dupa",
+      },
+      {
+        itemPrice: 100, //tu trzeba z bazy pobrac?
+        amount: 3,
+        id: 1,
+        title: "dupa",
+      },
+      {
+        itemPrice: 100, //tu trzeba z bazy pobrac?
+        amount: 3,
+        id: 1,
+        title: "dupa",
+      }
+    ]
+  );
 
   const cart = {
     products: 0
@@ -22,20 +37,34 @@ export default function UserCart() {
 
   const handlePrice = () => {
     let ans = 0;
-    ans += (item.amount * item.itemPrice);
+    ans += (items.amount * items.itemPrice);
     setPrice(ans);
   }
 
   const handleAdding = () => {
-    let add = item.amount;
+    let add = items.amount;
     add += 1;
     setAmount(add);
   } 
 
   const handleDeleting = () => {
-    let del = item.amount;
+    let del = items.amount;
     del -= 1;
     setAmount(del);
+  }
+
+
+  function Item() {
+    return (
+      <Stack direction="row" spacing={2}>
+                          <img width="70" height="70"  src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2"/>
+                          <p>{item.title}</p>
+                          <p>{item.itemPrice}</p>
+                          <Button onClick={handleAdding} className="bg-sky-500" variant="contained"> + </Button>
+                          <p>{item.amount}</p>            
+                          <Button onClick={handleDeleting} className="bg-sky-500" variant="contained"> - </Button>
+                      </Stack>
+    );
   }
 // const handleRemoving
 
@@ -44,21 +73,20 @@ export default function UserCart() {
   
     return (
       <>
-        <Grid container direction="row" my={4} justifyContent="center" alignItems="flex-start" spacing={20} >
-                <div className="cart_img">
-                  <img width="50" height="50"  src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2"/>
-                  <p>{item.title}</p>
-                  <p>{item.itemPrice}</p>
-                </div>
-            <div>
-              <Button onClick={handleAdding} className="bg-sky-500" variant="contained"> + </Button>
-              <p>{item.amount}</p>            
-              <Button onClick={handleDeleting} className="bg-sky-500" variant="contained"> - </Button>
-            </div> 
-        <div className='total'>
-          <span> Podsumowanie Twojego koszyka</span>
-          <span> = {price}</span>
-        </div>
+        <Grid container direction="row" my={4} justifyContent="center" alignItems="flex-start" spacing={2} >
+                
+                  <Stack spacing={2} alignItems="center">
+                    <Stack direction="row" spacing={2}>
+                      {items.map((item) => {
+                          <Item itemPrice={ProductsList.itemPrice}></Item>
+                      })}
+                    </Stack>
+                      <div className='total'>
+                        <span> Podsumowanie Twojego koszyka</span>
+                        <span> = {price}</span>
+                      </div>
+                    </Stack>
+        
         </Grid>
         </>
     );
