173287b71c9bd5370b4b562b4031f6aaedb3f535
Natabyc <nataliabyc01@gmail.com>
2024-01-23 15:49:22 +0100

    todo: object update (amount on button click & total price)

--- client/src/components/shared/userCart/UserCart.tsx
+++ client/src/components/shared/userCart/UserCart.tsx
@@ -1,91 +1,81 @@
 import { Grid, Button, Stack, TextField} from '@mui/material';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function UserCart() {
-  //zorbic z mapem zeby byla lista
 
-  const [items, setItems] = useState(
-    [
+    const [cartTotal, setCartTotal] = useState(0);
+    const items = [
       {
         itemPrice: 100, //tu trzeba z bazy pobrac?
         amount: 3,
         id: 1,
         title: "dupa",
+        imgURL: "https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2",
       },
       {
         itemPrice: 100, //tu trzeba z bazy pobrac?
         amount: 3,
-        id: 1,
+        id: 2,
         title: "dupa",
+        imgURL: "https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2",
       },
       {
         itemPrice: 100, //tu trzeba z bazy pobrac?
         amount: 3,
-        id: 1,
+        id: 3,
         title: "dupa",
+        imgURL: "https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2",
       }
-    ]
-  );
-
-  const cart = {
-    products: 0
-  }
+    ];
+    const [itemList, setItemList] = useState(items);
   
-  const [price, setPrice] = useState(0);
-  const [amount, setAmount] = useState(item.amount); 
+  const [quanity, setQty] = useState(0); 
+    
+    useEffect(() => {
+      total();
+    }, [items]);
   
+    const total = () => {
+      let totalVal = 0;
+        // eslint-disable-next-line @typescript-eslint/prefer-for-of
+        for (let i = 0; i < items.length; i++) {
+          {
+            totalVal += items[i].itemPrice;
+          }
+      setCartTotal(totalVal);
+      };
+    }
 
-  const handlePrice = () => {
-    let ans = 0;
-    ans += (items.amount * items.itemPrice);
-    setPrice(ans);
-  }
+    const handleQty= () =>{
+      setQty(prevState=>prevState+1)
+    } //not working
 
-  const handleAdding = () => {
-    let add = items.amount;
-    add += 1;
-    setAmount(add);
-  } 
+  const listItems = items.map((el) => 
+      <li key={el.id}>
+        <Stack direction="row" spacing={2}>
+      <p> {el.title} </p>
+      <p> {el.itemPrice} zł </p>
+      <Button className="bg-sky-500" variant="contained" style={{maxWidth: '30px', maxHeight: '30px'}} onClick={()=>handleQty}>+</Button>
+      <p> {el.amount} </p>
+      <Button className="bg-sky-500" variant="contained" style={{maxWidth: '30px', maxHeight: '30px'}} >-</Button>
+      <img src={el.imgURL} width={60} height={60} />
+      </Stack>
+      </li>
+  ); 
+    
 
-  const handleDeleting = () => {
-    let del = items.amount;
-    del -= 1;
-    setAmount(del);
-  }
-
-
-  function Item() {
-    return (
-      <Stack direction="row" spacing={2}>
-                          <img width="70" height="70"  src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2"/>
-                          <p>{item.title}</p>
-                          <p>{item.itemPrice}</p>
-                          <Button onClick={handleAdding} className="bg-sky-500" variant="contained"> + </Button>
-                          <p>{item.amount}</p>            
-                          <Button onClick={handleDeleting} className="bg-sky-500" variant="contained"> - </Button>
-                      </Stack>
-    );
-  }
-// const handleRemoving
-
-
-
-  
     return (
       <>
         <Grid container direction="row" my={4} justifyContent="center" alignItems="flex-start" spacing={2} >
-                
-                  <Stack spacing={2} alignItems="center">
-                    <Stack direction="row" spacing={2}>
-                      {items.map((item) => {
-                          <Item itemPrice={ProductsList.itemPrice}></Item>
-                      })}
-                    </Stack>
-                      <div className='total'>
-                        <span> Podsumowanie Twojego koszyka</span>
-                        <span> = {price}</span>
-                      </div>
-                    </Stack>
+            <Stack spacing={2} alignItems="center">
+              <Stack direction="row" spacing={2}>
+               <ul>{listItems}</ul>
+              </Stack>
+                <div className='total'>
+                  <span> Podsumowanie Twojego koszyka</span>
+                  <span> = {cartTotal} zł</span>
+                </div>
+            </Stack>
         
         </Grid>
         </>
