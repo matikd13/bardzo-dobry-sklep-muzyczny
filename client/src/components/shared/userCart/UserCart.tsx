12c2123ae6dc2d9c6f91435c1baa0dacac78e6a9
Natabyc <nataliabyc01@gmail.com>
2024-01-24 23:32:02 +0100

    nie dziala :(((

--- client/src/components/shared/userCart/UserCart.tsx
+++ client/src/components/shared/userCart/UserCart.tsx
@@ -1,5 +1,5 @@
-import { Grid, Button, Stack, TextField} from '@mui/material';
-import { useState, useEffect } from 'react';
+import { Grid, Button, Stack, TextField, listItemClasses} from '@mui/material';
+import { useState, useEffect, useContext, useReducer } from 'react';
 
 export default function UserCart() {
 
@@ -13,49 +13,56 @@ export default function UserCart() {
         imgURL: "https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2",
       },
       {
-        itemPrice: 100, //tu trzeba z bazy pobrac?
+        itemPrice: 200, 
         amount: 3,
         id: 2,
         title: "dupa",
         imgURL: "https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2",
       },
       {
-        itemPrice: 100, //tu trzeba z bazy pobrac?
+        itemPrice: 100, 
         amount: 3,
         id: 3,
         title: "dupa",
         imgURL: "https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2",
       }
     ];
-    const [itemList, setItemList] = useState(items);
-  
-  const [quanity, setQty] = useState(0); 
-    
-    useEffect(() => {
-      total();
-    }, [items]);
-  
-    const total = () => {
-      let totalVal = 0;
-        // eslint-disable-next-line @typescript-eslint/prefer-for-of
-        for (let i = 0; i < items.length; i++) {
-          {
-            totalVal += items[i].itemPrice;
-          }
-      setCartTotal(totalVal);
-      };
-    }
 
-    const handleQty= () =>{
-      setQty(prevState=>prevState+1)
-    } //not working
+    const handlePrice = ()=>{
+      
+      let ans = 0;
+      // eslint-disable-next-line @typescript-eslint/prefer-for-of
+      for (let i = 0; i < items.length; i++) {
+        items.map((item)=> {
+            ans += items[i].amount * items[i].itemPrice;
+          }
+        )};
+      setCartTotal(ans);
+  }
+      //kurwa mnoży razy 3 xd
+      
+      const handleChange = () => {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        let ans = items[id].amount;
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        ans += items[id].amount;
+
+      }
+
+      useEffect(() => {
+        handlePrice();
+      }, [items]);
+
+      //to dodawanie tych produktow trzeba zrobic :((
+  
+  
 
   const listItems = items.map((el) => 
       <li key={el.id}>
         <Stack direction="row" spacing={2}>
       <p> {el.title} </p>
       <p> {el.itemPrice} zł </p>
-      <Button className="bg-sky-500" variant="contained" style={{maxWidth: '30px', maxHeight: '30px'}} onClick={()=>handleQty}>+</Button>
+      <Button className="bg-sky-500" variant="contained" style={{maxWidth: '30px', maxHeight: '30px'}} >+</Button>
       <p> {el.amount} </p>
       <Button className="bg-sky-500" variant="contained" style={{maxWidth: '30px', maxHeight: '30px'}} >-</Button>
       <img src={el.imgURL} width={60} height={60} />
@@ -84,6 +91,9 @@ export default function UserCart() {
 
 }             
 
-    
 
-  
+
+
+
+
+
