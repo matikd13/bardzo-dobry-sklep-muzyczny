b92a6cf90c4c598fcd3bbf2e78d4628ad6520cb1
Natabyc <nataliabyc01@gmail.com>
2024-01-25 14:59:46 +0100

    adding and substracting and price is working!!

--- client/src/components/shared/userCart/UserCart.tsx
+++ client/src/components/shared/userCart/UserCart.tsx
@@ -4,56 +4,78 @@ import { useState, useEffect, useContext, useReducer } from 'react';
 export default function UserCart() {
 
     const [cartTotal, setCartTotal] = useState(0);
-    const items = [
+    const [items, setItems] = useState([
       {
         itemPrice: 100, //tu trzeba z bazy pobrac?
-        amount: 3,
+        itemAmount: 0,
         id: 1,
         title: "dupa",
         imgURL: "https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2",
       },
       {
         itemPrice: 200, 
-        amount: 3,
+        itemAmount: 0,
         id: 2,
         title: "dupa",
         imgURL: "https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2",
       },
       {
         itemPrice: 100, 
-        amount: 3,
+        itemAmount: 0,
+        id: 3,
+        title: "dupa",
+        imgURL: "https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2",
+      },
+      {
+        itemPrice: 100, 
+        itemAmount: 0,
         id: 3,
         title: "dupa",
         imgURL: "https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2",
       }
-    ];
+    ]);
+
+    useEffect(() => {
+      //handleAdding(items: id);
+      handlePrice();
+      //handleSubstracting(items[id]);
+    }, [items]);
 
     const handlePrice = ()=>{
       
       let ans = 0;
       // eslint-disable-next-line @typescript-eslint/prefer-for-of
-      for (let i = 0; i < items.length; i++) {
-        items.map((item)=> {
-            ans += items[i].amount * items[i].itemPrice;
-          }
-        )};
+      for (let i = 0; i < items.length; i++) 
+        ans += items[i].itemAmount * items[i].itemPrice;
       setCartTotal(ans);
   }
-      //kurwa mnoży razy 3 xd
+      //dlaczego mnoży razy 3 xd bo mnozy razy ilosc elementow w koszyku - w ktorym miejscu? DZIAŁA!!!!!
       
-      const handleChange = () => {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-        let ans = items[id].amount;
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-        ans += items[id].amount;
-
+      const handleAdding = (id: number) => {
+        //1. find the item with provided id
+        const currentAmountIndex = items.findIndex((el) => el.id === id );
+        //2. change the amount
+        const updatedAmount = Object.assign({}, items[currentAmountIndex]);
+        updatedAmount.itemAmount += 1;
+        //3. update the items list with updated amount
+        const newItems = items.slice();
+        newItems[currentAmountIndex] = updatedAmount;
+        setItems(newItems);
       }
 
-      useEffect(() => {
-        handlePrice();
-      }, [items]);
+      const handleSubstracting = (id: number) => {
+        //1. find the item with provided id
+        const currentAmountIndex = items.findIndex((el) => el.id === id );
+        //2. change the amount
+        const updatedAmount = Object.assign({}, items[currentAmountIndex]);
+        updatedAmount.itemAmount -= 1;
+        //3. update the items list with updated amount
+        const newItems = items.slice();
+        newItems[currentAmountIndex] = updatedAmount;
+        setItems(newItems);
+      }
 
-      //to dodawanie tych produktow trzeba zrobic :((
+     
   
   
 
@@ -62,9 +84,9 @@ export default function UserCart() {
         <Stack direction="row" spacing={2}>
       <p> {el.title} </p>
       <p> {el.itemPrice} zł </p>
-      <Button className="bg-sky-500" variant="contained" style={{maxWidth: '30px', maxHeight: '30px'}} >+</Button>
-      <p> {el.amount} </p>
-      <Button className="bg-sky-500" variant="contained" style={{maxWidth: '30px', maxHeight: '30px'}} >-</Button>
+      <Button className="bg-sky-500" variant="contained" style={{maxWidth: '30px', maxHeight: '30px'}} onClick={() => handleAdding(el.id)} >+</Button>
+      <p> {el.itemAmount} </p>
+      <Button className="bg-sky-500" variant="contained" style={{maxWidth: '30px', maxHeight: '30px'}} onClick={() => handleSubstracting(el.id)} >-</Button>
       <img src={el.imgURL} width={60} height={60} />
       </Stack>
       </li>
@@ -82,6 +104,7 @@ export default function UserCart() {
                   <span> Podsumowanie Twojego koszyka</span>
                   <span> = {cartTotal} zł</span>
                 </div>
+                <Button className="bg-sky-500" variant="contained" style={{maxWidth: '300px', maxHeight: '30px'}}>Przejdź do płatności</Button>
             </Stack>
         
         </Grid>
